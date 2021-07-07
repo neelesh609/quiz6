45e1cccc91264216eed319034dec8c2c35a1cd4c
Rithika Ramasesha <65168737+neelesh609@users.noreply.github.com>
2021-07-07 15:09:31 -0500

    small update

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, session,g
 from flask_socketio import SocketIO, join_room, leave_room, emit
-from flask_session import Session
 
 
 application=Flask(__name__)
